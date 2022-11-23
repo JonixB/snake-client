5781e30811a7b7b99b6a8d1874186053ff196a15
Nikko Bansil <bjohnnikko@gmail.com>
2022-11-23 15:01:06 -0800

    separated the constants into a module

--- client.js
+++ client.js
@@ -1,10 +1,11 @@
+const { host, port} = require('./constants');
 // establishes a connection with the game server
 const net = require("net");
 
 const connect = function () {
   const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: 50541,
+    host,
+    port,
   });
 
   // interpret incoming data as text
